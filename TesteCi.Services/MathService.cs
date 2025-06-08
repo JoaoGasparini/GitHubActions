28f70cb430653d1184550745ab930590a1063de5
JoaoNunes <JoaoNunes@DESKTOP-P7BM2PM>
2025-06-08 20:33:12 -0300

    teste

---
+++ TesteCi.Services/MathService.cs
@@ -0,0 +1,12 @@
+﻿using TesteCi.Services.Domain.Interfaces;
+
+namespace TesteCi.Services
+{
+    public class MathService : IMathService
+    {
+        public int GetDouble(int number)
+        {
+            return number * 2;
+        }
+    }
+}
---
+++ TesteCi/Controllers/MathController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TesteCi.Services.Domain.Interfaces;
+
+namespace TesteCi.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MathController : ControllerBase
+    {
+        private readonly IMathService _mathService;
+
+        public MathController(IMathService mathService)
+        {
+            _mathService = mathService;
+        }
+
+        [HttpGet("double/{number}")]
+        public ActionResult<int> GetDouble(int number)
+        {
+            return Ok(_mathService.GetDouble(number));
+        }
+    }
+}
